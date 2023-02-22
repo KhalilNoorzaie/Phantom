7a28c22208da5a6acee305e7f0683b51d4288750
unknown <Khalil.Noorzaie@tekschool.us>
2023-02-22 16:46:18 -0500

    this is class practice

--- src/test/git/GitTest.java
+++ src/test/git/GitTest.java
@@ -3,6 +3,10 @@ package test.git;
 public class GitTest {
 	public void test() {
 		System.out.println("this is just a test");
+		
 	}
 
+	public void testtwo() {
+		System.out.println("this is class practice");
+	}
 }
