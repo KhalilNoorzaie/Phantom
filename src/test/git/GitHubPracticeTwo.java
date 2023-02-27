db3ba914662d8c68d4cb32eec5bee1238ed78fe2
unknown <Khalil.Noorzaie@tekschool.us>
2023-02-27 14:31:39 -0500

    This is Today

--- src/test/git/GitHubPracticeTwo.java
+++ src/test/git/GitHubPracticeTwo.java
@@ -1,9 +1,9 @@
 package test.git;
 
 public class GitHubPracticeTwo {
-	public void getstwo(int a , int b) {
-		int result = a*b;
-		System.out.println(result);
+	public int getstwo(int a , int b) {
+		return a*b;
+		
 	}
 
 }
